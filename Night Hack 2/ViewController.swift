caf8645e705c04797a6ae5d6681db0dfc409d69c
Christina <christinayao698@gmail.com>
2022-07-14 22:38:57 -0700

    Initial Commit

---
+++ Night Hack 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Night Hack 2
+//
+//  Created by Christina Yao on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
